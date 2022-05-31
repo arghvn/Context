03e7f44254b63bc788b00f31149f949c9ea6179a
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2022-05-31 16:21:27 +0430

    comments and outputs

--- Context.go
+++ Context.go
@@ -1,4 +1,12 @@
+//Note: This exercise is very related to the topic of channels
+// and goroutines and is done only to get acquainted with the
+// http protocol, so in the future, sections related to channels and goroutines will be developed.
+
 package main
+
+//HTTP servers are useful for demonstrating the usage of context.Context for controlling cancellation.
+// A Context carries deadlines, cancellation signals,
+//and other request-scoped values across API boundaries and goroutines.
 
 import (
 	"fmt"
@@ -8,14 +16,24 @@ import (
 
 func hello(w http.ResponseWriter, req *http.Request) {
 
+	//A context.Context is created for each request by the net/http machinery,
+	// and is available with the Context() method.
+
 	ctx := req.Context()
 	fmt.Println("server: hello handler started")
 	defer fmt.Println("server: hello handler ended")
+
+	//Wait for a few seconds before sending a reply to the client.
+	//This could simulate some work the server is doing. While working,
+	//keep an eye on the context’s Done() channel for a signal that we
+	// should cancel the work and return as soon as possible.
 
 	select {
 	case <-time.After(10 * time.Second):
 		fmt.Fprintf(w, "hello\n")
 	case <-ctx.Done():
+
+		//The context’s Err() method returns an error that explains why the Done() channel was closed.
 
 		err := ctx.Err()
 		fmt.Println("server:", err)
@@ -29,3 +47,11 @@ func main() {
 	http.HandleFunc("/hello", hello)
 	http.ListenAndServe(":8090", nil)
 }
+
+//output :
+
+//curl localhost:8090/hello
+//server: hello handler started
+//^C
+//server: context canceled
+//server: hello handler ended
